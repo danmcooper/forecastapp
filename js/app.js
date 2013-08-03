6782a9eced2110dedca9538a104dd88fc29c22a2
Dan Cooper <dan@trapit.com>
2013-08-02 19:14:35 -0700

    tabs to spaces

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
 $(function() {
-	$("#address-form").submit(function() {
-		// get city, sanitized
-		var address = APP.HELPERS.htmlEncode($("#address-input").val());
-		if (address.length > 0) {
-			// call display
-			APP.FORECAST.hide();
-			APP.MAP.display(address);
-		};
+    $("#address-form").submit(function() {
+        // get city, sanitized
+        var address = APP.HELPERS.htmlEncode($("#address-input").val());
+        if (address.length > 0) {
+            // call display
+            APP.FORECAST.hide();
+            APP.MAP.display(address);
+        };
 
-		return false;
-	});
+        return false;
+    });
 
-	$(document.body).on('geoUpdated', function() {
-		// geo data has been updated - get forecast
-		var geo = APP.MAP.getGeo();
-		var lat = geo.lat();
-		var lng = geo.lng();
-		var key = APP.HELPERS.htmlEncode($("#key-input").val());
-		if (key.length > 0) {
-			APP.FORECAST.get(lat, lng, key);
-		} else {
-			alert("need api key");
-			$("#key-input").focus();
-		}
-	});
+    $(document.body).on('geoUpdated', function() {
+        // geo data has been updated - get forecast
+        var geo = APP.MAP.getGeo();
+        var lat = geo.lat();
+        var lng = geo.lng();
+        var key = APP.HELPERS.htmlEncode($("#key-input").val());
+        if (key.length > 0) {
+            APP.FORECAST.get(lat, lng, key);
+        } else {
+            alert("need api key");
+            $("#key-input").focus();
+        }
+    });
 
 
-	$(document.body).on('forecastUpdated', function() {
-		// have forecast data - draw forecast
-		APP.FORECAST.draw();
-	});	
+    $(document.body).on('forecastUpdated', function() {
+        // have forecast data - draw forecast
+        APP.FORECAST.draw();
+    }); 
 
-	// because who wants to have to click in the box to get focus?
-	$("#address-input").focus();
+    // because who wants to have to click in the box to get focus?
+    $("#address-input").focus();
 });
--- js/helpers.js
+++ js/helpers.js
@@ -2,31 +2,31 @@ var APP = APP || {};
 APP.HELPERS = APP.HELPERS || {};
 
 APP.HELPERS.WEATHERLOOKUPTABLE = 
-	{"rain": Skycons.RAIN,
- 	 "clear-day": Skycons.CLEAR_DAY,
- 	 "clear-night": Skycons.CLEAR_NIGHT,
- 	 "snow": Skycons.SNOW, 
- 	 "sleet": Skycons.SLEET, 
- 	 "wind": Skycons.WIND, 
- 	 "fog": Skycons.FOG, 
- 	 "cloudy": Skycons.CLOUDY, 
- 	 "partly-cloudy-day": Skycons.PARTLY_CLOUDY_DAY, 
- 	 "partly-cloudy-night": Skycons.PARTLY_CLOUDY_NIGHT};
+    {"rain": Skycons.RAIN,
+     "clear-day": Skycons.CLEAR_DAY,
+     "clear-night": Skycons.CLEAR_NIGHT,
+     "snow": Skycons.SNOW, 
+     "sleet": Skycons.SLEET, 
+     "wind": Skycons.WIND, 
+     "fog": Skycons.FOG, 
+     "cloudy": Skycons.CLOUDY, 
+     "partly-cloudy-day": Skycons.PARTLY_CLOUDY_DAY, 
+     "partly-cloudy-night": Skycons.PARTLY_CLOUDY_NIGHT};
 
 $.extend(APP.HELPERS, {
-	htmlEncode: function(value) {
-	  return $('<div/>').text(value).html();
-	},
-	htmlDecode: function(value) {
-	  return $('<div/>').html(value).text();
-	},
-	forecastIOToSkycons: function(forecastIOWeather) {
-		var weatherIcon = APP.HELPERS.WEATHERLOOKUPTABLE[forecastIOWeather];
+    htmlEncode: function(value) {
+        return $('<div/>').text(value).html();
+    },
+    htmlDecode: function(value) {
+        return $('<div/>').html(value).text();
+    },
+    forecastIOToSkycons: function(forecastIOWeather) {
+        var weatherIcon = APP.HELPERS.WEATHERLOOKUPTABLE[forecastIOWeather];
 
-		if (weatherIcon) {
-			return weatherIcon;
-		} else {
-			return "";
-		}
-	}	
+        if (weatherIcon) {
+            return weatherIcon;
+        } else {
+            return "";
+        }
+    }   
 });
